5ebb533319378d202d52b5075c165c82b6a87cd9
3dapi <emarhayyim@hotmail.com>
2025-06-30 10:56:56 +0900

    코드 수정
    # Conflicts:
    #	2d_engine/SceneGameMesh.cpp
    #	2d_engine/SceneGameMesh.h

--- 2d_engine/SceneGameMesh.h
+++ 2d_engine/SceneGameMesh.h
@@ -32,19 +32,25 @@ public:
 	int		Render()								override;
 
 protected:
-	void SetupUploadChain();
-	int	 UpdateUploadChain();
-
 	void UpdateCamera(const GameTimer& gt);
 	void BuildBox();
 	void UpdateBox(const GameTimer& gt);
     void DrawBox(ID3D12GraphicsCommandList* cmdList);
 
 public:
-	vector<unique_ptr<ShaderUploadChain>>	m_subLst	;
-	ShaderUploadChain*						m_subCur	{};
-	int										m_subIdx	{};
-	RenderItem			m_wireBox{};
+	unique_ptr<ShaderUploadChain>	m_cbUploader	{};
+	
+	// descriptor
+	ComPtr<ID3D12DescriptorHeap>	m_boxSrvDesc{};
+	D3D_PRIMITIVE_TOPOLOGY			m_boxPrimitive = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	// vertex buffer
+	G2::StaticResBufVtx				m_boxVtx{};
+	G2::StaticResBufIdx				m_boxIdx{};
+
+	// const buufer
+	ShaderConstTransform			m_boxCbTrs;
+	ShaderConstPass					m_boxCbPss;
+	ShaderConstMaterial				m_boxCbMtl;
 
 	XMFLOAT3	m_tmEyePos = { 0.0f, 0.0f, 0.0f };
 	float		mTheta = 1.5f*XM_PI;
